75293b5e696de5ae37b03e5c704613e624247a1f
Regis Ribeiro <regisgomesr@gmail.com>
2020-03-18 21:50:18 -0300

    Estilizando Adoptions

--- src/screens/Adoptions/index.js
+++ src/screens/Adoptions/index.js
@@ -1,12 +1,29 @@
 import React from 'react'
+import { Toast, ToastBody } from 'reactstrap';
+import { MdClear } from 'react-icons/md'; 
 
+import img6 from '../../assets/6.jpg'
+
+import './index.css'
 
 const Adoptions = props => {
     return(
         
-        <div className='home-container'>
+        <div className='adoption-container'>
               
-            <h1>Adoptions!</h1>
+            <h1>Lista de Adoção</h1>
+
+            <div className="p-3 my-2 rounded">
+                <Toast className='toast'>
+                    
+                    <ToastBody className='toastBody'>
+                        <img src={img6} alt="Image" height="60" width="60"/>
+                        <h4>Chow-chow</h4>
+                        <MdClear className='mdClear' size={24} color='#000' opacity='0.3'/>
+                    </ToastBody>
+                </Toast>
+            </div>
+
         </div>
     )
 }
--- src/screens/Breeds/index.js
+++ src/screens/Breeds/index.js
@@ -13,7 +13,7 @@ const Index = props => {
 
     return(
 
-        <div>
+        <div className='div'>
             <Header />
             <Route exact path={`/breeds`} component={Breeds} />
             <Route exact path={`${props.match.path}/:id/details`} component={DetailsBreed} />
